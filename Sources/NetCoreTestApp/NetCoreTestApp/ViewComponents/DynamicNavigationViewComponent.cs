b823d867b03520f90addebe24bd752055e5d2c66
Yauheniya Smolnikava <Yauheniya_Smolnikava@epam.com>
2018-11-04 20:41:15 +0300

    added view component and additional layout

---
+++ Sources/NetCoreTestApp/NetCoreTestApp/ViewComponents/DynamicNavigationViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreTestApp.ViewComponents.Models;
+using System.Threading.Tasks;
+
+namespace NetCoreTestApp.ViewComponents
+{
+    public class DynamicNavigationViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string entityName, string formName = "")
+        {
+            var model = new DynamicNavigationModel
+            {
+                ControllerName = entityName,
+                ActionName = formName
+            };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Sources/NetCoreTestApp/NetCoreTestApp/ViewComponents/Models/DynamicNavigationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreTestApp.ViewComponents.Models
+{
+    public class DynamicNavigationModel
+    {
+        public string ControllerName { get; set; }
+
+        public string ActionName { get; set; }
+    }
+}
