42ca893d148f438b05dba3a7c5795545962f3084
Yauheniya Smolnikava <Yauheniya_Smolnikava@epam.com>
2018-11-05 10:54:14 +0300

    Module 3, Task 3

---
+++ Sources/NetCoreTestApp/NetCoreTestApp/HtmlHelpers/ImageHtmlHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NetCoreTestApp.HtmlHelpers
+{
+    public static class ImageHtmlHelper
+    {
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper htmlHelper, int imageId, string linkText)
+           => new HtmlString($"<a href='/images/{imageId}'>{linkText}</a>");
+    }
+}
---
+++ Sources/NetCoreTestApp/NetCoreTestApp/TagHelpers/NorthwindIdTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NetCoreTestApp.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class NorthwindIdTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Add("href", $"/images/{context.AllAttributes["northwind-id"].Value}");
+            output.Attributes.RemoveAll("northwind-id");
+        }
+    }
+}
