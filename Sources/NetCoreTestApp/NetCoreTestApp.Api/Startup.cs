005fc7eef102e946a323e36e313630a6ad964189
Yauheniya Smolnikava <Yauheniya_Smolnikava@epam.com>
2018-11-18 18:28:26 +0300

    Module 4, Task 4 added swagger

--- Sources/NetCoreTestApp/NetCoreTestApp.Api/Startup.cs
+++ Sources/NetCoreTestApp/NetCoreTestApp.Api/Startup.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using NetCoreTestApp.DataAccess.Interfaces;
 using NetCoreTestApp.DataAccess.Models;
 using NetCoreTestApp.DataAccess.Repositories;
+using Swashbuckle.AspNetCore.Swagger;
+
 
 namespace NetCoreTestApp.Api
 {
@@ -30,6 +25,10 @@ namespace NetCoreTestApp.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
             var connection = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(connection));
             services.AddScoped<IProductsRepository, ProductsRepository>();
@@ -51,6 +50,11 @@ namespace NetCoreTestApp.Api
             app.UseHttpsRedirection();
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseMvc();
         }
     }
